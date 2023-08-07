1cae9392837d9934d669369b0ea649ce47b0b7d2
brandon-nguyen-lam <brandonnguyenlam@gmail.com>
2023-08-07 01:15:57 -0400

    💚 new underline and trying new thing

--- components/footer.js
+++ components/footer.js
@@ -57,7 +57,7 @@ const Footer = () => {
             fontSize={'sm'}
             color={'black'}
           >
-            site last updated 8/3/2023
+            site last updated 8/7/2023
           </Box>
         </Link>
       </Container>
--- components/logo.js
+++ components/logo.js
@@ -1,16 +1,59 @@
-import Link from 'next/link'
-import { Box, Text, Image, HStack } from '@chakra-ui/react'
-const Logo = () => {
-  return (
-    <Link href={'/'} scroll={false}>
-      <HStack>
-        <Image src={'/images/peepoSit.png'} width={10} height={10} />
-        <Text fontFamily={'M PLUS Rounded 1c'} fontSize={'xl'} color={'black'}>
-          Brandon Lam
-        </Text>
-      </HStack>
-    </Link>
-  )
+import { useState } from 'react';
+import NextLink from 'next/link';
+import { Box, Text, Image, HStack } from '@chakra-ui/react';
+
+function AnimatedLink({ href, color, children }) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const linkStyle = {
+        color,
+        textDecoration: 'none',
+        position: 'relative',
+    };
+
+    const underlineStyle = {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: isHovered ? '100%' : '0%',
+        height: '2px',
+        background: '#5F967C',
+        transition: 'width 0.3s ease-in-out',
+    };
+
+    return (
+        <NextLink href={href} passHref>
+            <Box
+                style={linkStyle}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                {children}
+                <span style={underlineStyle} />
+            </Box>
+        </NextLink>
+    );
 }
+
+const Logo = () => {
+    return (
+        <HStack>
+            <Image src={'/images/peepoSit.png'} width={10} height={10} />
+            <AnimatedLink href={'/'} color="black">
+                <Text fontFamily={'M PLUS Rounded 1c'} fontSize={'xl'} color={'black'}>
+                    Brandon Lam
+                </Text>
+            </AnimatedLink>
+        </HStack>
+    )
+};
 
 export default Logo
